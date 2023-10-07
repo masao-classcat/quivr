bbd2c29680a5a126fcbb89569d9766f19a280788
masao <sales-info@classcat.com>
2023-10-07 13:53:24 +0900

    Revert "feat: add onboarding_a column to onboarding table (#1340)"
    This reverts commit 16cea2c86bc6cfbe99a9b70bfbd94e7879bf329e.

--- backend/models/databases/supabase/onboarding.py
+++ backend/models/databases/supabase/onboarding.py
@@ -12,19 +12,14 @@ class OnboardingUpdatableProperties(BaseModel):
 
     """Properties that can be received on onboarding update"""
 
-    onboarding_a: Optional[bool]
     onboarding_b1: Optional[bool]
     onboarding_b2: Optional[bool]
     onboarding_b3: Optional[bool]
 
-    class Config:
-        extra = "forbid"
 
-
-class OnboardingStates(BaseModel):
+class GetOnboardingResponse(BaseModel):
     """Response when getting onboarding"""
 
-    onboarding_a: bool
     onboarding_b1: bool
     onboarding_b2: bool
     onboarding_b3: bool
@@ -34,18 +29,13 @@ class Onboarding(Repository):
     def __init__(self, supabase_client):
         self.db = supabase_client
 
-    def get_user_onboarding(self, user_id: UUID) -> OnboardingStates | None:
+    def get_user_onboarding(self, user_id: UUID) -> GetOnboardingResponse | None:
         """
         Get user onboarding information by user_id
         """
         onboarding_data = (
-            self.db.from_("onboardings")
-            .select(
-                "onboarding_a",
-                "onboarding_b1",
-                "onboarding_b2",
-                "onboarding_b3",
-            )
+            self.db.from_("onboarding")
+            .select("user_id", "onboarding_b1", "onboarding_b2", "onboarding_b3")
             .filter("user_id", "eq", user_id)
             .limit(1)
             .execute()
@@ -54,19 +44,15 @@ class Onboarding(Repository):
         if onboarding_data == []:
             return None
 
-        return OnboardingStates(**onboarding_data[0])
+        return GetOnboardingResponse(**onboarding_data[0])
 
     def update_user_onboarding(
         self, user_id: UUID, onboarding: OnboardingUpdatableProperties
-    ) -> OnboardingStates:
+    ) -> GetOnboardingResponse:
         """Update user onboarding information by user_id"""
-        update_data = {
-            key: value for key, value in onboarding.dict().items() if value is not None
-        }
-
         response = (
-            self.db.from_("onboardings")
-            .update(update_data)
+            self.db.from_("onboarding")
+            .update(onboarding.dict())
             .match({"user_id": user_id})
             .execute()
             .data
@@ -75,4 +61,4 @@ class Onboarding(Repository):
         if not response:
             raise HTTPException(404, "User onboarding not updated")
 
-        return OnboardingStates(**response[0])
+        return GetOnboardingResponse(**response[0])
