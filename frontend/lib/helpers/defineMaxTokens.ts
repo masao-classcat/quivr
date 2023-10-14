b7ad7b6dbd47335cc57deb514d65c5224f8be500
masao <masao@classcat.com>
2023-10-14 12:49:16 +0900

    localization

--- frontend/lib/helpers/defineMaxTokens.ts
+++ frontend/lib/helpers/defineMaxTokens.ts
@@ -1,15 +1,16 @@
 import { Model, PaidModels } from "../types/brainConfig";
 
+// masao : 14-oct-23 : localization
 export const defineMaxTokens = (model: Model | PaidModels): number => {
   //At the moment is evaluating only models from OpenAI
   switch (model) {
     case "gpt-3.5-turbo":
-      return 750;
+      return 1500;   // 750;
     case "gpt-3.5-turbo-16k":
-      return 2000;
+      return 4000;   // 2000;
     case "gpt-4":
-      return 1000;
+      return 2000;   // 1000;
     default:
-      return 500;
+      return 1000;   // 500;
   }
 };
