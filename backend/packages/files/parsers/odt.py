8c411d5c051af92d14bc919e0b2e90e06316b75a
masao <masao@classcat.com>
2023-11-30 21:03:17 +0900

    release 0.0.105 fixed

--- backend/packages/files/parsers/odt.py
+++ backend/packages/files/parsers/odt.py
@@ -4,7 +4,6 @@ from models import File
 from .common import process_file
 
 
-def process_odt(file: File, enable_summarization, brain_id, user_openai_api_key):
 def process_odt(file: File, brain_id):
     return process_file(
         file=file,
