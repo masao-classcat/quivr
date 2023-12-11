b811210bf400e16fc459ff59f20ff39a78649702
masao <masao@classcat.com>
2023-12-11 19:34:36 +0900

    localization

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -5,15 +5,12 @@ import { useSupabase } from "@/lib/context/SupabaseProvider";
 import { redirectToPreviousPageOrChatPage } from "@/lib/helpers/redirectToPreviousPageOrChatPage";
 
 import {
-  DemoSection,
   FooterSection,
   HomeHeader,
   HomeSection,
   IntroSection,
-  SecuritySection,
 } from "./components";
 import { HomeHeaderBackground } from "./components/HomeHeader/components/HomeHeaderBackground";
-import { UseCases } from "./components/UseCases/UseCases";
 
 const HomePage = (): JSX.Element => {
   const { session } = useSupabase();
@@ -35,23 +32,6 @@ const HomePage = (): JSX.Element => {
       >
         <HomeSection bg="transparent">
           <IntroSection />
-        </HomeSection>
-
-        <HomeSection bg="bg-[#FCFAF6]" slantAfter="down" hiddenOnMobile={true}>
-          <DemoSection />
-        </HomeSection>
-
-        <HomeSection
-          bg="bg-[#362469]"
-          slantCurrent="down"
-          gradient="bg-gradient-to-t bg-gradient-to-t from-white to-[#362469]"
-        >
-          <UseCases />
-          <div />
-        </HomeSection>
-
-        <HomeSection bg="bg-white" slantBefore="down" slantAfter="up">
-          <SecuritySection />
         </HomeSection>
 
         <HomeSection
